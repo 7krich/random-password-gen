05ef11fc82fa66c22fa7dc5bafa09cf0ecee34dc
Kyle Richnafsky <7krich@gmail.com>
2022-02-20 18:23:36 -0500

    added password generation functionality

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
 // Assignment code here
-//const cardl = document.getElementById('card');
-//const uppercaseEl = document.getElementById('uppercase');
-//const lowercaseEl = document.getElementById('lowercase');
-//const numbersEl = document.getElementById('numbers');
-//const symbolsEl = document.getElementById('symbols');
-//const generateEl = document.getElementById('generate');
-
-
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -27,57 +16,28 @@ function writePassword() {
 
 }
 
-//random character functions
-function getRandomLower () {
-  var confirmLowercase = window.confirm('Would you like the password to include lowercase letters?');
+function generatePassword() {
+  let randomPassword = ''
+  const randomCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12345678910!@#%$%^&*()'
+  const windowLength = window.prompt('What is your desired password length?');
 
-  if (confirmLowercase) {
-    window.alert('Lowercase letters will be included in your random password!');
-  return String.fromCharCode(Math.floor(Math.random() * 26 +97));
-  } 
-  return false;
-}
+  var promptLowercase = window.confirm('Do you want to include lowercase letters in your password?');
 
-function getRandomUpper () {
-  var confirmUppercase = window.confirm('Would you like the password to include uppercase letters?');
-
-  if (confirmUppercase) {
-    window.alert('Uppercase letters will be included in your random password!');
-  return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-  } 
-  return false;
-}
-
-function getRandomNumber () {
-  var confirmNumbers = window.confirm('Would you like the password to include numbers?');
-
-  if (confirmNumbers) {
-    window.alert('Numbers will be included in your random password!');
-  return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+  if (promptLowercase) {
+    window.alert('Lowercase letters will be included in your password!');
   }
-  return false;
-}
 
-function getRandomSymbol () {
-  var confirmSymbols = window.confirm('Would you like the password to include symbols?');
-
-  if (confirmSymbols) {
-    window.alert('Symbols will be included in your random password!');
-  const symbols = "!@#%$%^&*()"
-  return symbols[Math.floor(Math.random() * symbols.length)];
+  if (!promptLowercase) {
+    randomCharacters.replaceAll(/[a-z]/g, '')
   }
-  return false;
+  console.log(randomCharacters);
+  console.log(promptLowercase);
+  for (let i = 0; i < Number(windowLength); i ++) {
+    randomPassword += randomCharacters.charAt(Math.floor(Math.random() * randomCharacters.length
+    ))
+  }
+  return randomPassword;
 }
 
-console.log(getRandomLower());
-console.log(getRandomUpper());
-console.log(getRandomNumber());
-console.log(getRandomSymbol());
 
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword {
- // const hasLower = lowercaseEl.confirm;
-  //const hasUpper = uppercaseEl.confirm;
-  //const hasNumber = numbersEl.confirm;
-  //const hasSymbol = symbolsEl.confirm;
-//});
+
