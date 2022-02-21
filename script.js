a41da452dec009b11bb42bfefc885da164dc8171
Kyle Richnafsky <7krich@gmail.com>
2022-02-20 21:13:57 -0500

    completed pasword generator

--- script.js
+++ script.js
@@ -18,8 +18,13 @@ function writePassword() {
 
 function generatePassword() {
   let randomPassword = ''
-  const randomCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12345678910!@#%$%^&*()'
+  let randomCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12345678910!@#%$%^&*()'
   const windowLength = window.prompt('What is your desired password length?');
+
+  if (Number(windowLength) < 5 || Number(windowLength) > 128) {
+    window.alert('You must choose a number between 5 and 128.');
+    generatePassword();
+  }
 
   var promptLowercase = window.confirm('Do you want to include lowercase letters in your password?');
 
@@ -28,8 +33,46 @@ function generatePassword() {
   }
 
   if (!promptLowercase) {
-    randomCharacters.replaceAll(/[a-z]/g, '')
+    window.alert('Lowercase letters will not be included in your password!');
+    randomCharacters = randomCharacters.replaceAll(/[a-z]/g, '');
   }
+
+  var promptUppercase = window.confirm('Do you want to include uppercase letters in your password?');
+
+  if (promptUppercase) {
+    window.alert('Uppercase letters will be included in your password!');
+  }
+
+  if (!promptUppercase) {
+    window.alert('Uppercase letters will not be included in your password!');
+    randomCharacters = randomCharacters.replaceAll(/[A-Z]/g, '');
+  }
+
+  var promptNumber = window.confirm('Do you want to include numbers in your password?');
+
+  if (promptNumber) {
+    window.alert('Numbers letters will be included in your password!');
+  }
+
+  if (!promptNumber) {
+    window.alert('Numbers will not be included in your password!');
+    randomCharacters = randomCharacters.replaceAll(/[0-9]/g, '');
+  }
+
+  var promptSpecial = window.confirm('Do you want to include special characters in your password?');
+
+  if (promptSpecial) {
+    window.alert('Special characters letters will be included in your password!');
+  }
+
+  if (!promptSpecial) {
+    window.alert('Special Characters will not be included in your password!');
+    randomCharacters = randomCharacters.replaceAll(/[^A-Za-z0-9]/g, '');
+  }
+
+
+
+  
   console.log(randomCharacters);
   console.log(promptLowercase);
   for (let i = 0; i < Number(windowLength); i ++) {
