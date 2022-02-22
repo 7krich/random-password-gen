ab3c89a3d4ba4676629be4c7d37d96ab230841d6
Kyle Richnafsky <7krich@gmail.com>
2022-02-21 19:33:50 -0500

    Min characters changed, reduced prompts and updated README.md

--- README.md
+++ README.md
@@ -20,11 +20,8 @@ https://7krich.github.io/random-password-gen/
 <img width="1440" alt="Screen Shot 2022-02-21 at 8 48 43 AM" src="https://user-images.githubusercontent.com/97630036/154968040-6e2f9535-d666-44ea-aece-e3f61b3c5d90.png">
 <img width="1440" alt="Screen Shot 2022-02-21 at 8 49 08 AM" src="https://user-images.githubusercontent.com/97630036/154968048-0e1e6347-9627-4fed-85fd-2d7d24b3f748.png">
 <img width="1440" alt="Screen Shot 2022-02-21 at 8 49 17 AM" src="https://user-images.githubusercontent.com/97630036/154968051-24bb5bcd-e202-496c-b45e-f6d3b957bb83.png">
-<img width="1440" alt="Screen Shot 2022-02-21 at 8 49 25 AM" src="https://user-images.githubusercontent.com/97630036/154968054-ecc84602-7b73-4686-ad86-25a5f74f1858.png">
 <img width="1440" alt="Screen Shot 2022-02-21 at 8 49 33 AM" src="https://user-images.githubusercontent.com/97630036/154968057-e38cf843-84f4-470e-88fa-6c5a25f4f06b.png">
-<img width="1440" alt="Screen Shot 2022-02-21 at 8 49 42 AM" src="https://user-images.githubusercontent.com/97630036/154968058-a2a787c3-be2c-42c0-a302-6060b55b2af7.png">
 <img width="1440" alt="Screen Shot 2022-02-21 at 8 57 23 AM" src="https://user-images.githubusercontent.com/97630036/154969067-71a30961-85f6-4069-827b-0c0f55baf551.png">
-<img width="1440" alt="Screen Shot 2022-02-21 at 8 49 52 AM" src="https://user-images.githubusercontent.com/97630036/154968059-2714f3bf-d34c-44cf-9c44-03dda33a4351.png">
 <img width="1440" alt="Screen Shot 2022-02-21 at 8 49 59 AM" src="https://user-images.githubusercontent.com/97630036/154968062-3f18f079-a23a-4c45-9f92-0854d0f71989.png">
-<img width="1440" alt="Screen Shot 2022-02-21 at 8 50 07 AM" src="https://user-images.githubusercontent.com/97630036/154968064-5fd7b2ef-0106-450f-8b0d-b821787b0d29.png">
+<img width="1438" alt="Screen Shot 2022-02-21 at 7 30 04 PM" src="https://user-images.githubusercontent.com/97630036/155042258-e3db21d2-059b-44b1-ae9c-386dfcb8b85c.png">
 <img width="1440" alt="Screen Shot 2022-02-21 at 8 50 16 AM" src="https://user-images.githubusercontent.com/97630036/154968066-a87a3bde-242c-413b-ab8e-bfe5e99761d1.png">
--- script.js
+++ script.js
@@ -15,69 +15,72 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+ ;
 function generatePassword() {
   let randomPassword = ''
   let randomCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12345678910!@#%$%^&*()'
   const windowLength = window.prompt('What is your desired password length?');
 
-  if (Number(windowLength) < 5 || Number(windowLength) > 128) {
-    window.alert('You must choose a number between 5 and 128.');
+  if (Number(windowLength) < 8 || Number(windowLength) > 128) {
+    window.alert('You must choose a number between 8 and 128.');
   }
 
   if (!/^\d+$/.test(windowLength)) {
-    window.alert('You typed in a letter. You must choose a number between 5 and 128.');
+    window.alert('You typed in a letter. You must choose a number between 8 and 128.');
     return generatePassword(); 
   }
   console.log(/^\d+$/.test(windowLength));
 
-  if (Number(windowLength) < 5 || Number(windowLength) > 128) {
+  if (Number(windowLength) < 8 || Number(windowLength) > 128) {
     return generatePassword();
   }
 
   var promptLowercase = window.confirm('Do you want to include lowercase letters in your password?');
 
   if (promptLowercase) {
-    window.alert('Lowercase letters will be included in your password!');
+    var promptLowercase = ('lowercase letters, ');
   }
 
   if (!promptLowercase) {
-    window.alert('Lowercase letters will not be included in your password!');
+    var promptLowercase = ('');
     randomCharacters = randomCharacters.replaceAll(/[a-z]/g, '');
   }
 
   var promptUppercase = window.confirm('Do you want to include uppercase letters in your password?');
 
   if (promptUppercase) {
-    window.alert('Uppercase letters will be included in your password!');
+    var promptUppercase = ('uppercase letters, ');
   }
 
   if (!promptUppercase) {
-    window.alert('Uppercase letters will not be included in your password!');
+    var promptUppercase = ('');
     randomCharacters = randomCharacters.replaceAll(/[A-Z]/g, '');
   }
 
   var promptNumber = window.confirm('Do you want to include numbers in your password?');
 
   if (promptNumber) {
-    window.alert('Numbers will be included in your password!');
+    var promptNumber = ('numbers, ');
   }
 
   if (!promptNumber) {
-    window.alert('Numbers will not be included in your password!');
+    var promptNumber = ('');
     randomCharacters = randomCharacters.replaceAll(/[0-9]/g, '');
   }
 
   var promptSpecial = window.confirm('Do you want to include special characters in your password?');
 
   if (promptSpecial) {
-    window.alert('Special characters will be included in your password!');
+    var promptSpecial = ('special characters');
   }
 
   if (!promptSpecial) {
-    window.alert('Special Characters will not be included in your password!');
+    var promptSpecial = ('');
     randomCharacters = randomCharacters.replaceAll(/[^A-Za-z0-9]/g, '');
   }
+  
+  window.alert('Your password will be ' + windowLength + ' characters long. It will also include the following: ' + promptUppercase + promptLowercase + promptNumber + promptSpecial + '.');
+  
   
   console.log(randomCharacters);
   console.log(promptLowercase);
